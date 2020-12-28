2b0d6283614d9c8415bcd65b2d412605f2281753
Anil Gangapersaud <a.gangapersaud@gmail.com>
2020-12-27 19:42:41 -0500

    Added Database

--- api/routes/images.js
+++ api/routes/images.js
@@ -1,57 +1,89 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Image = require('../models/image');
 
 /**
- * Gets all images from the server.
+ * Returns a list of all images in the database.
  */
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /images'
+    Image.find().exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+
+/**
+ * Returns a single JSON object representing an image from the database.
+ */
+router.get('/:imageId', (req, res, next) => {
+    const imageId = req.params.imageId;
+    Image.findById(imageId)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'No valid entry found for provided ID'
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err: err.message});
     });
 });
 
 /**
- * Uploads a single image to the server.
+ * Uploads a single image to the database.
  */
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Handling POST requests to /images'
+    const image = new Image({
+        _id:  new mongoose.Types.ObjectId(),
+        name: req.body.name
+    });
+    image.
+    save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /images',
+            uploadedImage: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 /**
- * Gets a single image from the server based on the filename.
+ * Deletes an image from the sever based on the image id.
  */
-router.get('/:filename', (req, res, next) => {
-    const filename = req.params.filename;
-    if (filename === 'nezuko') {
-        res.status(200).json({
-            message: 'Retrieved image Nezuko',
-            id: filename
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed a filename'
-        });
-    }
-});
-
-/**
- * Deletes an image from the sever based on the filename.
- */
-router.delete('/:filename', (req,res,next) => {
-    const filename = req.params.filename;
-    if (filename === 'nezuko') {
-        res.status(200).json({
-            message: 'Deleted image nezuko',
-            id: filename
-        });
-    } else {
-        res.status(200).json({
-            message: 'Failed to delete image',
-            id: filename
-        });
-    }
+router.delete('/:imageId', (req,res,next) => {
+    const id = req.params.imageId;
+    Image.remove({_id: id}).exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 /**
  * Sends all incoming requests to the images route.
  */
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const imageRoutes = require('./api/routes/images');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Log any requests
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Give access to any client to avoid CORS errors
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+//         return res.status(200).json({});
+//     }
+// })
+
+// Routes which should handle requests
 app.use('/images', imageRoutes);
+
+// Handle errors
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;

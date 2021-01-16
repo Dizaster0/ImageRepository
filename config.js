773feadde509747b893389085e03944b07105789
Dizaster0 <a.gangapersaud@gmail.com>
2021-01-15 19:30:09 -0500

    Added config file, and upload folder

---
+++ config.js
@@ -0,0 +1,4 @@
+const mongoURI = "mongodb+srv://Dizaster:K1NGD0M@dizaster.2fpmc.mongodb.net/ImageRepository?authSource=admin&replicaSet=Dizaster-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+const port = 3000;
+
+module.exports =  { mongoURI, port }
